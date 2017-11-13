535cf9d7ad9c07cdfeeeb7f4a5eca5796e12fb00
Kasper Lewau <lewauu@gmail.com>
2017-11-13 14:45:31 +0100

    un-es6

--- js/twitter-text.js
+++ js/twitter-text.js
@@ -1238,12 +1238,14 @@ const unorm = require('unorm');
 
   function weightForCodePoint (codePoint) {
     // { start, end, weight }
-    const inRange = config.ranges.find(range => {
+    var inRange = config.ranges.find(function (range) {
       return codePoint >= range.start && codePoint <= range.end;
     });
+
     if (!inRange) {
       return config.defaultWeight;
     }
+
     return inRange.weight;
   }
   // Returns the length of Tweet text with consideration to t.co URL replacement
